85c2fb53612dcd73c44444ecdab40e9550a43293
duc1503 <riruru212@gmail.com>
2023-08-03 08:53:56 +0700

    update-mausac

--- 1_DAL/IRepository/IMauSacRepository.cs
+++ 1_DAL/IRepository/IMauSacRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1_DAL.DomainMolder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace _1_DAL.IRepository
 {
-	internal interface IMauSacRepository
+	public interface IMauSacRepository
 	{
-	}
+        public bool Add(MauSac obj);
+        public bool Update(MauSac obj);
+        public bool Delete(MauSac obj);
+        public List<MauSac> GetAll();
+    }
 }
