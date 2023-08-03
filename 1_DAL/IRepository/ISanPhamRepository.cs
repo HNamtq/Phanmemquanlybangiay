50e7c5b833fae0d3a11e54b7bcea8e2b5514b2fd
duc1503 <riruru212@gmail.com>
2023-08-04 05:11:28 +0700

    update-sanpham

--- 1_DAL/IRepository/ISanPhamRepository.cs
+++ 1_DAL/IRepository/ISanPhamRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1_DAL.DomainMolder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace _1_DAL.IRepository
 {
-	internal interface ISanPhamRepository
+	public interface ISanPhamRepository
 	{
-	}
+        public bool Add(SanPham obj);
+        public bool Update(SanPham obj);
+        public bool Delete(SanPham obj);
+        public List<SanPham> GetAll();
+    }
 }
--- 2_BUS/IServices/ISanPhamServices.cs
+++ 2_BUS/IServices/ISanPhamServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1_DAL.DomainMolder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace _2_BUS.IServices
 {
-	internal interface ISanPhamServices
+	public interface ISanPhamServices
 	{
-	}
+        bool Add(SanPham obj);
+        bool Update(SanPham obj);
+        bool Delete(SanPham obj);
+        List<SanPham> GetAll();
+        List<SanPham> GetAll(string input);
+    }
 }
